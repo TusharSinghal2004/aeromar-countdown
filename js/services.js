688cd77d42bb11581655d4929ee6f37460297ca2
TusharSinghal2004 <you@example.com>
2026-06-18 23:27:42 +0530

    edits

---
+++ js/services.js
@@ -0,0 +1,19 @@
+const cards = document.querySelectorAll('.service-card');
+
+const observer = new IntersectionObserver(entries => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add('visible');
+
+        }
+
+    });
+
+},{
+    threshold:0.1
+});
+
+cards.forEach(card => observer.observe(card));
